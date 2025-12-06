d0e0e0044db411e5d32c6426ac095d00a169f09e
Rob Gage <rmg@oakleycomputing.org>
2025-12-05 21:08:13 -0500

    Add Compose README as introduction with gifs Build

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fce526c8.js',
+                path_to_root + 'searchindex-8631d6f1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- src/functions/sum.md
+++ src/functions/sum.md
@@ -4,4 +4,4 @@ Computes the sum of two integers on top of the stack.
 
 `5 10 +` → `15`
 
-`-500 200` → `-300`
+`-500 200 +` → `-300`
